7ed91cb5dce0b41da73ce69122b0a1377bb56666
vaibhav <vaibhav.waghmare@techprescient.com>
2024-01-25 16:27:36 +0530

    controller is added

---
+++ StudentManagement/src/main/java/com/Project/StudentManagement/controllerr.java
@@ -0,0 +1,9 @@
+package com.Project.StudentManagement;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controllerr {
+
+    // Writing something just for fun
+}
